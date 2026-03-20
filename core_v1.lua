8bfd12c112577513c838396ef3cad677182ad72b
hihizhopa-cmd <hihizhopa@gmail.com>
2026-03-21 00:25:05 +0500

    Update core_v1.lua

--- core_v1.lua
+++ core_v1.lua
@@ -1,1 +1,5 @@
-print("123")
+print("--- SYSTEM 18+ ONLINE ---")
+local p = Instance.new("Part", workspace)
+p.Name = "SuccessPart"
+p.Position = Vector3.new(0, 20, 0)
+p.Color = Color3.new(0, 1, 0)
